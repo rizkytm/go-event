708045562689be7303f3190bceaa41761b2947c9
Rizky TM <ritrime97@gmail.com>
2025-03-01 14:38:37 +0700

    feat: test create and get db

--- main.go
+++ main.go
@@ -19,7 +19,10 @@ func main() {
 }
 
 func getEvents(context *gin.Context) {
-	events := models.GetAllEvents()
+	events, err := models.GetAllEvents()
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "could not fetch"})
+	}
 	context.JSON(http.StatusOK, events)
 }
 
@@ -34,7 +37,11 @@ func createEvent(context *gin.Context) {
 
 	event.ID = 1
 	event.UserID = 1
-	event.Save()
+	err = event.Save()
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "could not create"})
+	}
 
 	context.JSON(http.StatusCreated, gin.H{"message": "Event created", "event": event})
 }
