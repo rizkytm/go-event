9cb31d3b18c94026022f5d52fcdeb2daf65b813b
Rizky TM <ritrime97@gmail.com>
2025-03-01 16:21:07 +0700

    feat: update event

--- routes/events.go
+++ routes/events.go
@@ -48,9 +48,42 @@ func createEvent(context *gin.Context) {
 	err = event.Save()
 
 	if err != nil {
-		context.JSON(http.StatusInternalServerError, gin.H{"message": "could not create"})
+		context.JSON(http.StatusBadRequest, gin.H{"message": "could not parse"})
 		return
 	}
 
 	context.JSON(http.StatusCreated, gin.H{"message": "Event created", "event": event})
+}
+
+func updateEvent(context *gin.Context) {
+	eventId, err := strconv.ParseInt(context.Param("id"), 10, 64)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "could not fetch"})
+		return
+	}
+
+	_, err = models.GetEventByID(eventId)
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "could not fetch"})
+		return
+	}
+
+	var updatedEvent models.Event
+	err = context.ShouldBindJSON(&updatedEvent)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "could not parse"})
+		return
+	}
+
+	updatedEvent.ID = eventId
+	err = updatedEvent.Update()
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "could not parse"})
+		return
+	}
+
+	context.JSON(http.StatusOK, gin.H{"message": "event updated"})
 }
