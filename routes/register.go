1fb64fa0efef6440a40c217d2afb5b5e65b7a867
Rizky TM <ritrime97@gmail.com>
2025-03-02 11:29:36 +0700

    feat: cancel registration

--- routes/register.go
+++ routes/register.go
@@ -27,11 +27,25 @@ func registerForEvent(context *gin.Context) {
 
 	if err != nil {
 		context.JSON(http.StatusInternalServerError, gin.H{"message": "could not register"})
+		return
 	}
 
 	context.JSON(http.StatusCreated, gin.H{"message": "registered"})
 }
 
 func cancelRegistration(context *gin.Context) {
+	userId := context.GetInt64("userId")
+	eventId, err := strconv.ParseInt(context.Param("id"), 10, 64)
 
+	var event models.Event
+	event.ID = eventId
+
+	err = event.CancelRegistration(userId)
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "could not delete registration"})
+		return
+	}
+
+	context.JSON(http.StatusOK, gin.H{"message": "cancelled"})
 }
